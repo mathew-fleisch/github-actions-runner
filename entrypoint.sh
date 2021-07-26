1bc5e348039dd9552a680020aad2616e4782778f
mathew-fleisch <mathew.fleisch@gmail.com>
2021-07-26 07:51:28 -0700

    refactored entrypoint from github.com/mathew-fleisch/linter-runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# shellcheck disable=SC2155,SC2086
+
+LABELS="${LABELS:-linter}"
+RUNNER_WORKDIR="${RUNNER_WORKDIR:-_work}"
 
 if [ -z "$GIT_PAT" ]; then
     echo "Missing environment variable github token (GIT_PAT)"
@@ -13,7 +17,7 @@ if [ -z "$GIT_REPO" ]; then
     exit 1
 fi
 
-# If a kubeconfig env var exists, set it as the default kubecofig
+# If a kubeconfig env var exists, set it as the default kubeconfig
 if [ -n "$KUBECONFIG_CONTENTS" ]; then
     mkdir -p ~/.kube
     echo "$KUBECONFIG_CONTENTS" > ~/.kube/config
@@ -31,27 +35,27 @@ esac
 # If the agent is installed in the docker container, a new version of the docker container needs to be published
 # everytime a new version of the agent is released.
 GIT_RUNNER_VERSION=$(curl -s https://api.github.com/repos/actions/runner/releases/latest | jq -r '.tag_name' | sed -e 's/^v//g')
-echo "latest version: ${GIT_RUNNER_VERSION}"
-curl -Ls https://github.com/actions/runner/releases/download/v${GIT_RUNNER_VERSION}/actions-runner-${OS}-${ARCH}-${GIT_RUNNER_VERSION}.tar.gz | tar -zx
-
-# Hacky workaround because the installation script doesn't include a flag for automation to not prompt a non-existent user
-sed -i 's/\$apt_get install/DEBIAN_FRONTEND=noninteractive $apt_get install/g' ./bin/installdependencies.sh
+echo "latest release: https://api.github.com/repos/actions/runner/releases/latest ${GIT_RUNNER_VERSION}"
+DOWNLOAD_URL="https://github.com/actions/runner/releases/download/v${GIT_RUNNER_VERSION}/actions-runner-${OS}-${ARCH}-${GIT_RUNNER_VERSION}.tar.gz"
+echo "Downloading: ${DOWNLOAD_URL}"
+echo "Extracting in: $(pwd)"
+curl -Ls "$DOWNLOAD_URL" | tar -zx
 
 # Run the dependency installation script
 sudo ./bin/installdependencies.sh
 
-REG_URL="https://api.github.com/repos/${GIT_OWNER}/${GIT_REPO}/actions/runners/registration-token"
-echo "Requesting registration URL at '${REG_URL}'"
+REGISTRATION_URL="https://api.github.com/repos/${GIT_OWNER}/${GIT_REPO}/actions/runners/registration-token"
+echo "Requesting registration URL at '${REGISTRATION_URL}'"
 
-payload=$(curl -sX POST -H "Authorization: token ${GIT_PAT}" ${REG_URL})
+payload=$(curl -sX POST -H "Authorization: token ${GIT_PAT}" ${REGISTRATION_URL})
 export RUNNER_TOKEN=$(echo $payload | jq -r '.token')
 
 ./config.sh \
-    --name $(hostname) \
-    --token ${RUNNER_TOKEN} \
-    --url https://github.com/${GIT_OWNER}/${GIT_REPO} \
-    --work ${RUNNER_WORKDIR} \
-    --labels ${LABELS} \
+    --name "$(hostname)" \
+    --token "${RUNNER_TOKEN}" \
+    --url "https://github.com/${GIT_OWNER}/${GIT_REPO}" \
+    --work "${RUNNER_WORKDIR}" \
+    --labels "${LABELS}" \
     --unattended \
     --replace
 
